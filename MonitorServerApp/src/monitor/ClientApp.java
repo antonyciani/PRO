eb0c180a79fdf2caa02b7dc2a03313e1a04a6ef4
Ciani Antony <antony.ciani@heig-vd.ch>
2016-05-29 20:01:55 +0200

    Added Javadoc

--- MonitorServerApp/src/monitor/ClientApp.java
+++ MonitorServerApp/src/monitor/ClientApp.java
@@ -5,12 +5,18 @@ import communication.SystemInfoRetrieverClient;
 import communication.SystemInfoRetrieverProtocol;
 
 /**
+ * Cette classe est le programme principal de l'application côté client.
+ * Elle se charge d'envoyer les informations du poste sur lequel elle s'exécute
+ * au côté serveur.
+ * 
  * @author CIANI Antony
  *
  */
 public class ClientApp {
 
 	/**
+	 * Programme principal
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
@@ -19,6 +25,7 @@ public class ClientApp {
 			SystemInfoRetrieverClient sirc = new SystemInfoRetrieverClient(SystemInfoRetrieverProtocol.UDP_PORT,
 					SystemInfoRetrieverProtocol.TCP_PORT);
 
+			// Le client est toujours en écoute
 			while (true) {
 				System.out.println("Listening to server");
 				sirc.startListening();
